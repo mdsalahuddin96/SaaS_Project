77ab293c538b4ecbc1855f307ddbeb97c10bccf4
mdsalahuddin96 <salauddincse96@gmail.com>
2026-07-18 13:01:05 +0600

    create config env.js config file to validate env variable

---
+++ src/config/env.js
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const envSchema = z.object({
+  PORT: z.string().default('5000'),
+  MONGO_URI: z.string().url({ message: "Invalid Mongo DB URL format" }),
+  // JWT_SECRET: z.string().min(8),
+});
+
+const envServer = envSchema.safeParse(process.env);
+
+if (!envServer.success) {
+  console.error("Invalid environment variables:", envServer.error.format());
+  process.exit(1); 
+}
+
+export const env = envServer.data;
